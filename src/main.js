87d75ef42b1ca4e723c41c310858fe211cdee0ac
dhllonger <15720826387@139.com>
2025-04-18 15:43:34 +0800

    initial commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import axios from 'axios'
+
+const instance0 = axios.create({
+  baseURL: 'http://localhost:808'
+})
+Vue.prototype.$http = instance0
+
 
 Vue.config.productionTip = false
 
